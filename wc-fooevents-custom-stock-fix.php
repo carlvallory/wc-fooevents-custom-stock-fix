2f8d431384fbc03d967a20f86935e8d0a193d68f
Carlos Vallory <carlvallory@gmail.com>
2025-05-12 14:44:57 -0300

    change some requirements

--- wc-fooevents-custom-stock-fix.php
+++ wc-fooevents-custom-stock-fix.php
@@ -2,8 +2,17 @@
 /** 
 * Plugin Name: FooEvents Auto Refund for Canceled Orders
 * Description: Cambia automáticamente el estado de pedidos cancelados a devueltos si contienen entradas de FooEvents.
-* Version: 2.0
+* Version: 2.1
 * Author: Carlos Vallory
+* Developer: Carlos Vallory
+* License: GPL-3.0+
+* License URI: https://www.gnu.org/licenses/gpl-3.0.txt
+* Requires plugin: woocommerce
+* WP stable tag: 6.5.0
+* WP requires at least: 6.5.0
+* WP tested up to: 6.8.1
+* WC requires at least: 9.2.0
+* WC tested up to: 9.8.4
 */
 
 add_action('woocommerce_order_status_changed', 'convertir_cancelado_a_devuelto_si_es_evento_v2', 10, 4);
